e8b4345567021746fbb30ba49a319ca7b134b10b
vrfjr <vagnerrfjr@gmail.com>
2022-11-08 16:28:14 -0300

    1.0

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -5,10 +5,121 @@ using System.Threading.Tasks;
 using dio_projetos_c_.ExemploFundamentos.Common.Models;
 
 
+List<string> listaString = new List<string>();
+
+
+    listaString.Add("SP");
+    listaString.Add("BA");
+    listaString.Add("MG");
+
+Console.WriteLine($"Itens na minha lista {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+listaString.Add("SC");
+
+Console.WriteLine($"Itens na minha lista {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+listaString.Remove("MG");
+
+Console.WriteLine($"Itens na minha lista {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("Percorrendo o Lista com o For");
+// for(int contador = 0; contador < listaString.Count; contador++)
+// {
+
+//     Console.WriteLine($"Posição Nº {contador} - {listaString[contador]}");
+// }
+
+// Console.WriteLine("Percorrendo o Lista com o Foreach");
+// int contadorForeach = 0;
+// foreach (string item in listaString)
+// {
+//     Console.WriteLine($"Posição Nº {contadorForeach} - {item}");
+//      contadorForeach++;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  int[] arrayInteiros = new int [4];
+
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 64;
+// arrayInteiros[2] = 50;
+// arrayInteiros[3] = 1;
+
+
+// int[] arrayInteirosDobrado = new int(arrayInteiros.Length * 2);
+// Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+
+// // Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
+
+// Console.WriteLine("Percorrendo o Array com o For");
+// for(int contador = 0; contador < arrayInteiros.Length; contador++)
+// {
+
+//     Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
+//  }
+
+
+
+
+
+// Console.WriteLine("Percorrendo com o foreach");
+
+// int contadorForeach = 0;
+// foreach(int valor in arrayInteiros )
+// {
+
+//     Console.WriteLine($"Posição Nº {contadorForeach} - {valor}");
+//     contadorForeach++;
+// }
+
+
+
+
+
+
+
+
 // Pessoa pessoal = new Pessoa();
 // pessoal.Nome = "Vagner";
-// pessoal.Idade = 29;
-// pessoal.Apresentar();
+//  pessoal.Idade = 29;
+//  pessoal.Apresentar();
 
 
 
